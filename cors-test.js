b87a815925c4075da5ae58c8cda9c65e1b3d3bac
Jitender <jitendersandhu@users.noreply.github.com>
2016-08-26 12:55:38 -0700

    Update cors-test.js

--- cors-test.js
+++ cors-test.js
@@ -3,7 +3,7 @@
 (function() {
   console.log(window);
   window.makeError = function(a) {
-    a.b();
+    return a.b();
   };
   // window.onerror = function(msg, url, line, col, err) {
   //   console.log(msg + url + line + col);
