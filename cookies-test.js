11d3a818f7874ee769674f0d5b827c70d89152e1
Jitender <jitendersandhu@users.noreply.github.com>
2016-09-14 13:12:44 -0700

    Update cookies-test.js

--- cookies-test.js
+++ cookies-test.js
@@ -1,9 +1,9 @@
-// var name = 'TestCookieThirdParty', value = '23423', ttl = 23424, path = '/', domain = '.rawgit.com', secure = true;
-// document.cookie = name + "=" + escape(value) +
-// 			(ttl ? "; expires=" + new Date(+new Date()+(ttl*1000)).toUTCString() : "") +
-// 			(path   ? "; path=" + path : "") +
-// 			(domain ? "; domain=" + domain : "") +
-// 			(secure ? "; secure" : "")
+var name = 'TestCookieThirdParty', value = '23423', ttl = 23424, path = '/', domain, secure;
+document.cookie = name + "=" + escape(value) +
+			(ttl ? "; expires=" + new Date(+new Date()+(ttl*1000)).toUTCString() : "") +
+			(path   ? "; path=" + path : "") +
+			(domain ? "; domain=" + domain : "") +
+			(secure ? "; secure" : "")
 
-// document.cookie('TestCookieThirdParty');
+console.log('Cookie Found: ');
 console.log(/TestCookieThirdParty/.test(document.cookie));
