727505c095e026deb902d066a55e4983081c0151
Jitender <jitendersandhu@users.noreply.github.com>
2016-09-14 12:36:24 -0700

    Update cookie-test.js

--- cookie-test.js
+++ cookie-test.js
@@ -1,1 +1,7 @@
-document.cookie = "TestCookieThirdParty=1001";
+// document.cookie = "TestCookieThirdParty=1001";
+var name = 'TestCookieThirdParty', value = '23423', ttl = 23424, path = '/', domain = '.github.com', secure;
+document.cookie = name + "=" + escape(value) +
+			(ttl ? "; expires=" + new Date(+new Date()+(ttl*1000)).toUTCString() : "") +
+			(path   ? "; path=" + path : "") +
+			(domain ? "; domain=" + domain : "") +
+			(secure ? "; secure" : "")
