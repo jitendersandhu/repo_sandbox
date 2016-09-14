048967186cbdd2a1baba6830139293a060431505
Jitender <jitendersandhu@users.noreply.github.com>
2016-09-14 12:38:18 -0700

    Update and rename cookie-test.js to cookie-tests.js

--- cookie-tests.js
+++ cookie-tests.js
@@ -1,4 +1,3 @@
-// document.cookie = "TestCookieThirdParty=1001";
 var name = 'TestCookieThirdParty', value = '23423', ttl = 23424, path = '/', domain = '.github.com', secure;
 document.cookie = name + "=" + escape(value) +
 			(ttl ? "; expires=" + new Date(+new Date()+(ttl*1000)).toUTCString() : "") +
