72d39b2e3b125704a104434e2bf722f0998fa96f
Jitender <jitendersandhu@users.noreply.github.com>
2016-09-14 12:47:38 -0700

    Update and rename cookie-test.js to cookies-test.js

--- cookies-test.js
+++ cookies-test.js
@@ -1,4 +1,4 @@
-var name = 'TestCookieThirdParty', value = '23423', ttl = 23424, path = '/', domain = '.rawgit.com', secure;
+var name = 'TestCookieThirdParty', value = '23423', ttl = 23424, path = '/', domain = '.rawgit.com', secure = true;
 document.cookie = name + "=" + escape(value) +
 			(ttl ? "; expires=" + new Date(+new Date()+(ttl*1000)).toUTCString() : "") +
 			(path   ? "; path=" + path : "") +
